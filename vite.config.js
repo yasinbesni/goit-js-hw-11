68873dd595c50a252f6212fd69b2bfaae2cfae5d
Yasin Besni <125811137+yasinbesni@users.noreply.github.com>
2025-07-08 20:59:21 +0300

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,48 @@
-import { defineConfig } from 'vite';
-import { glob } from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
+import { defineConfig } from "vite";
+import { glob } from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
+import SortCss from "postcss-sort-media-queries";
 
 export default defineConfig(({ command }) => {
   return {
+    base: "/goit-js-hw-11/", // 🔥 Eklendi
     define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+      [command === "serve" ? "global" : "_global"]: {},
     },
-    root: 'src',
+    root: "src",
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync("./src/*.html"),
         output: {
           manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
+            if (id.includes("node_modules")) {
+              return "vendor";
             }
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
+          entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name === "commonHelpers") {
+              return "commonHelpers.js";
             }
-            return '[name].js';
+            return "[name].js";
           },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith(".html")) {
+              return "[name].[ext]";
             }
-            return 'assets/[name]-[hash][extname]';
+            return "assets/[name]-[hash][extname]";
           },
         },
       },
-      outDir: '../dist',
+      outDir: "../dist",
       emptyOutDir: true,
     },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/**.html']),
+      FullReload(["./src/**/**.html"]),
       SortCss({
-        sort: 'mobile-first',
+        sort: "mobile-first",
       }),
     ],
   };
